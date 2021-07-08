0cc62186307f018a5be8200741f19065cb3c0d44
deukyoon <77385179+deukyoon@users.noreply.github.com>
2021-07-08 17:28:12 +0900

    Create boj1000.cpp

---
+++ boj1000.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(void)
+{
+	int val1, val2;
+
+	std::cin >> val1;
+
+	std::cin >> val2;
+
+	int result = val1 + val2;
+	std::cout << result << std::endl;
+	return 0;
+}
